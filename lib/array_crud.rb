96f9b64bc63e5fc4cc504f6abf499279f14f91f3
jontenthoff@hotmail.com <jontenthoff@hotmail.com>
2018-08-29 20:51:56 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -19,21 +19,22 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
+  pets = ["cat", "dog", "bird", "hamster"]
   
 end
 
 def remove_element_from_start_of_array(array)
-  
+  pets = ["cat", "dog", "bird", "hamster"]
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  pets = ["cat", "dog", "bird", "hamster"]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  pets = ["cat", "dog", "bird", "hamster"]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  pets = ["cat", "dog", "bird", "hamster"]
 end
