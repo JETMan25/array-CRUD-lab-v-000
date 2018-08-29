5c31bc8c20b5b6e02456ccdd21cc1e1cd9f4c2eb
jontenthoff@hotmail.com <jontenthoff@hotmail.com>
2018-08-29 20:50:02 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,7 +9,7 @@ end
 def add_element_to_end_of_array(array, element)
   pets = ["cat", "dog", "bird", "hamster"]
   
-  pets << "arrays"
+  pets << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
