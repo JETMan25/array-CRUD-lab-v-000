6a6b187a637cfbb76b516ae678c0ed7200f3d786
jontenthoff@hotmail.com <jontenthoff@hotmail.com>
2018-08-29 20:53:09 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -19,7 +19,7 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  pets = ["cat", "dog", "bird", "hamster", "arrays"]
+  pets = ["cat", "dog", "bird", "hamster", "arrays!"]
   ham_ster = pets.pop
 end
 
