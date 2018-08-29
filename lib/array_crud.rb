961f172fa4ad8e7978dfe4378649c1315acffe09
jontenthoff@hotmail.com <jontenthoff@hotmail.com>
2018-08-29 20:47:50 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,9 +1,9 @@
 def create_an_empty_array
-  ["cat", "dog", "bird", "hamster"]
+  []
 end
 
 def create_an_array
-  []
+  ["cat", "dog", "bird", "hamster"]
 end
 
 def add_element_to_end_of_array(array, element)
