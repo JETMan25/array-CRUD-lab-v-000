032430895799773aa08a7c803f48a85e751e4140
jontenthoff@hotmail.com <jontenthoff@hotmail.com>
2018-08-29 20:58:36 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -39,6 +39,6 @@ def retrieve_first_element_from_array(array)
 end
 
 def retrieve_last_element_from_array(array)
-  pets = ["cat", "dog", "bird", "hamster"]
+  pets = ["cat", "dog", "bird", "hamster", "arrays!"]
   pets.last 
 end
