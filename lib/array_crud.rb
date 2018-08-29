ce82fd542cd9a269a918aaac07073a94d6744d58
jontenthoff@hotmail.com <jontenthoff@hotmail.com>
2018-08-29 20:55:30 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -29,7 +29,7 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  pets = ["cat", "dog", "bird", "hamster"]
+  pets = ["cat", "dog", "bird", "hamster", "am"]
   
 end
 
